b42cd29223383647ea2d8d8bcbbaf1d0dd77d313
kalyan babu tangella <tkalyanbabu63@gmail.com>
2021-02-07 20:59:26 +0530

    Read update delete apis

--- functions/services/Memes/controller.js
+++ functions/services/Memes/controller.js
@@ -21,4 +21,40 @@ router.get("/read", (req, res) => {
     .catch((error) => console.error(error));
 });
 
+router.post("/create", (req, res) => {
+  db.collection("MEMES")
+    .doc("/" + req.body.id + "/")
+    .create({
+      memeId: req.body.id,
+      category: req.body.category,
+      movieName: req.body.movieName,
+      imageURL: req.body.metaInfo.imageURL,
+      name: req.body.metaInfo.name,
+    });
+  return res
+    .json({ message: `Successfully Created Meme` })
+    .catch((error) => console.error(error));
+});
+
+router.put("/update/:id", (req, res) => {
+  db.collection("MEMES").doc(req.params.id).update({
+    memeId: req.body.id,
+    category: req.body.category,
+    movieName: req.body.movieName,
+    imageURL: req.body.metaInfo.imageURL,
+    name: req.body.metaInfo.name,
+  });
+  return res
+    .json({ message: `Successfully Updated Meme` })
+    .catch((error) => console.error(error));
+});
+
+router.delete("/delete/:id", (req, res) => {
+  db.collection("MEMES").doc(req.params.id).delete();
+  return res
+    .status(200)
+    .json({ message: `Successfully Deleted Meme` })
+    .catch((error) => console.error(error));
+});
+
 module.exports = router;
